7d6f31ee75bf8038cd3643362dadd5666dae12b9
Lucas Bandeira <lucasbandeirai40@gmail.com>
2023-08-23 14:55:00 -0300

    folder change

--- src/main/java/com/borracheiros/projeto/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.borracheiros.projeto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.borracheiros.projeto.entities.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-    
-}
