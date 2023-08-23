e9b881e60c71411225d6a94dbc9714fcf1140af3
DaniloMaximiano <danilo.profile@hotmail.com>
2023-08-23 20:57:16 -0300

    Organizando_arquivos

--- src/main/java/com/borracheiros/projeto/users/UserController.java
+++ src/main/java/com/borracheiros/projeto/users/UserController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class UserController {
     
-    // @GetMapping("/")
-    // public String home(){
-    //     return "index";
-    // }
+     @GetMapping("/")
+     public String home(){
+        return "TelaPrincipal";
+     }
 
     @GetMapping("/cadastro")
     public String showCreateUserPage(){
