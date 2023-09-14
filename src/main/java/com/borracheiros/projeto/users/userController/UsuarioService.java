5975eea38aa3f6e0525703ef64f28a3dc53b4922
AmaureX <amauryeb@gmail.com>
2023-09-14 01:37:30 -0300

    add usuarioservice

---
+++ src/main/java/com/borracheiros/projeto/users/userController/UsuarioService.java
@@ -0,0 +1,32 @@
+package com.borracheiros.projeto.users.userController;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.borracheiros.projeto.users.UsuarioRepository;
+import com.borracheiros.projeto.users.entities.Usuario;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class UsuarioService {
+
+    @Autowired    
+    private UsuarioRepository usuarioRepository;
+
+    public void status(long id, boolean StatusUsuario){
+        
+        Optional<Usuario> usuarioOptional = usuarioRepository.findById(id);
+        if (usuarioOptional.isPresent()) {
+            Usuario usuario = usuarioOptional.get();
+            usuario.setStatusUsuario(StatusUsuario);
+            usuarioRepository.save(usuario);
+        } else {
+            throw new EntityNotFoundException("Impossivel mudar o status do usuario");
+        }
+
+    }
+    
+}
