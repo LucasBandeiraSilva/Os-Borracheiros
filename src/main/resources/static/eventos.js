928a3852de5550b32bc6474784b8bf75852c9a2d
AmaureX <amauryeb@gmail.com>
2023-10-15 21:53:16 -0300

    paginacao da lista de produto funcionando

--- src/main/resources/static/eventos.js
+++ src/main/resources/static/eventos.js
@@ -20,3 +20,48 @@ INPUT_BUSCA.addEventListener('keyup', () => {
         }
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const itemList = document.getElementById("iproduto");
+    const items = itemList.getElementsByTagName("tr");
+    const itemsPerPage = 10;
+    const totalPages = Math.ceil(items.length / itemsPerPage);
+    let currentPage = 1;
+
+    function showPage(page) {
+        for (let i = 0; i < items.length; i++) {
+            if (i >= (page - 1) * itemsPerPage && i < page * itemsPerPage) {
+                items[i].style.display = "block";
+            } else {
+                items[i].style.display = "none";
+            }
+        }
+    }
+
+    function updatePaginationButtons() {
+        const prevPageButton = document.getElementById("prevPage");
+        const nextPageButton = document.getElementById("nextPage");
+
+        prevPageButton.disabled = currentPage === 1;
+        nextPageButton.disabled = currentPage === totalPages;
+    }
+
+    showPage(currentPage);
+    updatePaginationButtons();
+
+    document.getElementById("prevPage").addEventListener("click", function () {
+        if (currentPage > 1) {
+            currentPage--;
+            showPage(currentPage);
+            updatePaginationButtons();
+        }
+    });
+
+    document.getElementById("nextPage").addEventListener("click", function () {
+        if (currentPage < totalPages) {
+            currentPage++;
+            showPage(currentPage);
+            updatePaginationButtons();
+        }
+    });
+});
